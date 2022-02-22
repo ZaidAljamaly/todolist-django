5a62b3617be74f81756d42dabbef0294058952fb
ZaidAljamaly <81776430+ZaidAljamaly@users.noreply.github.com>
2022-02-23 00:38:41 +0300

    Update settings.py

--- taskslist/settings.py
+++ taskslist/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
