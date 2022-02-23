2934d761c35f9a622c5229852c353fa7a92e929b
ZaidAljamaly <81776430+ZaidAljamaly@users.noreply.github.com>
2022-02-23 09:53:32 +0300

    Update settings.py

--- taskslist/settings.py
+++ taskslist/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
